46602e6537f00a6d0a82f46c0fba7b4c002a291a
richterndzh <richterndzh@gmail.com>
2022-03-13 08:35:03 +0300

    lesson20

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,24 +1,10 @@
+import { animation } from "./helpers";
+
 const modal = () => {
   const modal = document.querySelector('.popup');
   const buttons = document.querySelectorAll('.popup-btn');
-  const closeBtn = modal.querySelector('.popup-close');
   const modalContent = modal.querySelector('.popup-content');
-  let animation;
-  let count = -70;
   let widthScreen = window.screen.width;
-
-  function appear () {
-      count++;
-      animation = requestAnimationFrame(appear);
-      if (count > 20)
-      {
-        cancelAnimationFrame(animation);
-      }
-      else
-      {
-        modalContent.style.top = count * 6 + 'px';
-      }
-  }
 
   buttons.forEach(btn => {
     btn.addEventListener('click', () => {
@@ -29,15 +15,26 @@ const modal = () => {
       else
       {
         modal.style.display = 'block';
-        appear();
+        animation({
+          duration: 1000,
+          timing(timeFraction)
+          {
+            return timeFraction;
+          },
+          draw(progress)
+          {
+            modalContent.style.left = (40 * progress) + '%';
+          }
+        });
       }
     });
   });
 
-  closeBtn.addEventListener('click', () => {
-    modal.style.display = 'none';
-    count = -70;
-  });
+  modal.addEventListener('click', (e) => {
+      if(!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) {
+         modal.style.display = 'none';
+      }
+   });
 };
 
 export default modal;
